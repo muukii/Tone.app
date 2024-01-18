4d63de0a5000a2d98dcce93069d608ab7e4d472a
Hiroshi Kimura <muukii.app@gmail.com>
2024-01-18 22:39:50 +0900

    Add MigrationPlan (#18)

--- Sources/AppService/Schema/v1/Schemas.V1.swift
+++ Sources/AppService/Schema/v1/Schemas.V1.swift
@@ -1,4 +1,11 @@
+import SwiftData
 
 extension Schemas {
-  public enum V1 {}
+  public enum V1: VersionedSchema {
+    public static var versionIdentifier: Schema.Version = .init(1, 0, 0)
+    public static var models: [any PersistentModel.Type] = [
+      PinEntity.self,
+      ItemEntity.self
+    ]
+  }
 }
