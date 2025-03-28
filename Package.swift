c9598208fc157120f7a98fee134dab630dd079d0
Muukii <muukii.app@gmail.com>
2025-03-28 18:54:08 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     .package(path: "submodules/FluidGroup/swift-dynamic-list"),
     .package(path: "submodules/FluidGroup/swiftui-ring-slider"),
     .package(url: "https://github.com/dagronf/SwiftSubtitles", from: "0.5.0"),
-    .package(url: "https://github.com/VergeGroup/Verge", branch: "main"),
+    .package(url: "https://github.com/VergeGroup/Verge", from: "14.0.0-beta.7"),
     .package(url: "https://github.com/FluidGroup/swiftui-support", branch: "main"),
     .package(url: "https://github.com/VergeGroup/Wrap", from: "4.0.0"),
     .package(url: "https://github.com/FluidGroup/MondrianLayout", branch: "main"),
